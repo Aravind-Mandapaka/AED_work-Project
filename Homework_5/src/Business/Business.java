279e4584147f08010f6023122c3f21078c2e12a1
Aravind <mandapaka.a@husky.neu.edu>
2017-10-15 22:18:17 -0400

    hw5

--- Homework_5/src/Business/Business.java
+++ Homework_5/src/Business/Business.java
@@ -5,10 +5,40 @@
  */
 package Business;
 
+import Business.HumanResources.PersonDirectory;
+import Business.SystemAdmin.UserAccountDirectory;
+
 /**
  *
  * @author aravind
  */
 public class Business {
+ 
+    private PersonDirectory personDirectory;
+    private UserAccountDirectory userAccountDirectory;
+
+    
+
+    public Business(String n) {
+         personDirectory= new PersonDirectory();
+        userAccountDirectory = new UserAccountDirectory();
+    }
+
+    public PersonDirectory getPersonDirectory() {
+        return personDirectory;
+    }
+
+    public void setPersonDirectory(PersonDirectory personDirectory) {
+        this.personDirectory = personDirectory;
+    }
+
+    public UserAccountDirectory getUserAccountDirectory() {
+        return userAccountDirectory;
+    }
+
+    public void setUserAccountDirectory(UserAccountDirectory userAccountDirectory) {
+        this.userAccountDirectory = userAccountDirectory;
+    }
+    
     
 }
--- Homework_5/src/Business/BusinessConfiguration/ConfigureABusiness.java
+++ Homework_5/src/Business/BusinessConfiguration/ConfigureABusiness.java
@@ -5,10 +5,41 @@
  */
 package Business.BusinessConfiguration;
 
+import Business.Business;
+import Business.HumanResources.Person;
+import Business.HumanResources.PersonDirectory;
+import Business.SystemAdmin.UserAccount;
+import Business.SystemAdmin.UserAccountDirectory;
+
 /**
  *
  * @author aravind
  */
 public class ConfigureABusiness {
     
+    
+    
+    
+    public static Business Initialize (String n) { // returns a business object 
+        Business b = new Business(n);
+        PersonDirectory pd = b.getPersonDirectory();
+        Person p = pd.addPerson(); //create person object
+        p.setFirstName(“Ann”);
+        p.setLastName(“Wells”);
+
+        Person p1 = pd.addPerson(); // create a second person object 
+        p1.setFirstName(“John”);
+        p1.setLastName(“Adam”);
+
+        UserAccountDirectory uad= b.getUserAccountDirectory(); // prepare to create user accounts
+        Person p2 = pd.findPersonByLastName(“Brown”); 
+        
+        If (p2!=null)
+        {
+        UserAccount ua = uad.addUserAccount();
+        ua.setPerson(p2); //link user account to the Mr. Brown ua.setUserId(“jadam”);
+        ua.setPassWard(“pw”);
+        ua.setRole(“System Admin”)
+        }
+    }
 }
